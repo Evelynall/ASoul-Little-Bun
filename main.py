fbadd2b555c634f916b12fb29daa7b6f083273ca
Evelynal <evelynalll@outlook.com>
2026-03-13 16:42:13 +0800

    添加开机自启的教程

--- main.py
+++ main.py
@@ -586,7 +586,7 @@ class ASoulLittleBun(QWidget):
     def show_about(self):
         """显示关于对话框"""
         about_text = """
-<h2>枝江小馒头 v1.1.0</h2>
+<h2>枝江小馒头 v1.0.3</h2>
 <p><b>By：</b>Evelynal</p>
 <p><b>B站：</b><a href="https://space.bilibili.com/33374590">伊芙琳娜</a></p>
 <p><b>开源地址：</b><a href="https://github.com/Evelynall/ASoul-Little-Bun/">ASoul-Little-Bun</a></p>
